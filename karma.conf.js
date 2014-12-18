9abde09e49c7accfaf04189fd5a177ce21011706
Kagami Hiiragi <kagami@genshiken.org>
2014-12-18 23:15:07 +0300

    Run tests on Firefox with karma as well

--- karma.conf.js
+++ karma.conf.js
@@ -50,14 +50,14 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ["chromium_no_sandbox"],
+    browsers: ["Chromium", "Firefox"],
     customLaunchers: {
-      chromium_no_sandbox: {
+      Chromium: {
         base: "Chrome",
         flags: ["--no-sandbox"],
       },
@@ -66,6 +66,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
   });
 };
